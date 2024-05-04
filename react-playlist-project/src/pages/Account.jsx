c29b4119f0a70e61c8fe0671c32bd181134d7534
kdurrence2 <kaitlynadurrence@gmail.com>
2024-05-03 21:38:05 -0400

    Account Page Template

--- react-playlist-project/src/pages/Account.jsx
+++ react-playlist-project/src/pages/Account.jsx
@@ -1,13 +1,47 @@
-import React from 'react'
-import '../App.css'
+import React from 'react';
 
-function Account() {
+function ProfilePage() {
+  const user = {
+    name: 'John Doe',
+    bio: 'Music lover | Guitarist | Singer',
+    favoriteGenres: ['Rock', 'Pop', 'Jazz'],
+    favoriteArtists: ['The Beatles', 'Taylor Swift', 'John Mayer'],
+    favoriteSongs: ['Bohemian Rhapsody', 'Shake It Off', 'Gravity'],
+  };
 
   return (
-    <>
-      <h1>My Account Page with Profile Information!</h1>
-    </>
-  )
+    <div>
+      <h1>Profile</h1>
+      <div>
+        <h2>{user.name}</h2>
+        <p>{user.bio}</p>
+      </div>
+      <div>
+        <h3>Favorite Genres</h3>
+        <ul>
+          {user.favoriteGenres.map((genre, index) => (
+            <li key={index}>{genre}</li>
+          ))}
+        </ul>
+      </div>
+      <div>
+        <h3>Favorite Artists</h3>
+        <ul>
+          {user.favoriteArtists.map((artist, index) => (
+            <li key={index}>{artist}</li>
+          ))}
+        </ul>
+      </div>
+      <div>
+        <h3>Favorite Songs</h3>
+        <ul>
+          {user.favoriteSongs.map((song, index) => (
+            <li key={index}>{song}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
 
-export default Account
+export default ProfilePage;
